c4c5d70c63e1f63526baa02a48400aa056ebdfe9
Derek Sparks <drsparks@gmail.com>
2020-04-23 19:54:48 -0700

    Initial Import

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string quitORnot = "";
+
+
+
+            do
+            {
+                Console.WriteLine("Hello! Please enter your name:");
+                string fName = Console.ReadLine();
+                if (fName.To = "")
+                Console.WriteLine($"Hello {fName}!  Please type exit at the prompt to quit. Type anything else to run again.");
+                quitORnot = Console.ReadLine();
+            } (quitORnot.ToLower("exit"));
+             
+            Console.ReadKey();
+        }
+    }
+}
